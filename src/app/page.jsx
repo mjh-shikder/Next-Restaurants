fa417c5161c7e14d649e689acbdd0c723bd39109
mjh-shikder <mjhsikder@gmail.com>
2026-01-07 13:58:41 +0600

    Project Created

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-5 min-h-screen justify-center text-center items-stretch">
+    
+    </div>
+  );
+}
