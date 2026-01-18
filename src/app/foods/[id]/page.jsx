2d36d628c055a84d21e4d8d3ddd33ffb173e74d1
mjh-shikder <mjhsikder@gmail.com>
2026-01-18 20:34:53 +0600

    Other meta data added

--- src/app/foods/[id]/page.jsx
+++ src/app/foods/[id]/page.jsx
@@ -7,7 +7,7 @@ export function generateStaticParams() {
 const getSingleFood = async (id) => {
   const res = await fetch(
     `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`,
-    { cache: "no-store" }
+    { cache: "no-store" },
   );
 
   if (!res.ok) return null;
@@ -16,20 +16,32 @@ const getSingleFood = async (id) => {
   return data.details;
 };
 
-export async function generateMetadata({params}) {
+export async function generateMetadata({ params }) {
   const { id } = await params;
-   const res = await fetch(
-     `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`,
-     { cache: "no-store" },
-   );
+  const res = await fetch(
+    `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`,
+    { cache: "no-store" },
+  );
 
-   if (!res.ok) return null;
+  if (!res.ok) return null;
 
-  const {details={}} = await res.json();
+  const { details = {} } = await res.json();
   return {
-    title: details.title
-  }
-  
+    title: details.title,
+    generator: "Next.js",
+    applicationName: "The Flamin Fork",
+    referrer: "origin-when-cross-origin",
+    keywords: ["Next.js", "React", "JavaScript"],
+    authors: [{ name: "Seb" }, { name: "Josh", url: "https://nextjs.org" }],
+    creator: "Jiachi Liu",
+    publisher: "Sebastian Markbåge",
+    formatDetection: {
+      email: false,
+      address: false,
+      telephone: false,
+    },
+    openGraph: ""
+  };
 }
 
 const ViewDetails = async ({ params }) => {
