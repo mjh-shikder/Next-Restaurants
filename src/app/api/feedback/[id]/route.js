fa4560eb12b5ceff2d6e02c26756b7e982c5aeb0
mjh-shikder <mjhsikder@gmail.com>
2026-01-19 20:30:31 +0600

    PATCH Api added

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -37,3 +37,37 @@ export async function DELETE(request, {params}) {
 
     return Response.json(result)
 }
+
+
+
+
+// Patch Function
+export async function PATCH(request, {params}) {
+    const { id } = await params;
+    const { message } = await request.json();
+
+    if (id.length != 24) {
+        return Response.json({
+            status: 400,
+            message: "Id not correct"
+        })
+    }
+
+    if (!message || typeof message !== "string") {
+        return Response.json({
+            status: 400,
+            message: "Send a Text Message"
+        })
+    }
+
+    const query = { _id: new ObjectId(id) }
+    const newData = {
+        $set: {
+            message
+        }
+    }
+
+   const result = await feedbackCollection.updateOne(query, newData)
+
+    return Response.json(result)
+}
