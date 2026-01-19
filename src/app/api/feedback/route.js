a10933c1cef3b970791d92215f389290eb158750
mjh-shikder <mjhsikder@gmail.com>
2026-01-19 16:37:08 +0600

    Connected to MongoDb

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,9 +1,14 @@
+import { connect } from "@/app/lib/dbConnect";
 import { feedback } from "../route";
 
 
+const feedbackCollection = connect("FirstNext")
 // Basic Get Fuction
 export async function GET(request) {
-    return Response.json(feedback)
+
+    const result = await feedbackCollection.find().toArray();
+    return Response.json(result)
+
 }
 
 // Basic Post function
@@ -18,11 +23,9 @@ export async function POST(request) {
         })
     }
 
-    const newFeedback = { message, id: feedback.length + 1 }
-    feedback.push(newFeedback);
+    const newFeedback = { message, date: new Date().toISOString };
+    
+    const result = await feedbackCollection.insertOne(newFeedback)
 
-    return Response.json({
-        acknowledgement: true,
-        insertedId: newFeedback.id,
-    })
+    return Response.json(result)
 }
