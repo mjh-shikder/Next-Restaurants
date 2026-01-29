4d86d244f883517c8081eb991a2b2d4133917652
mjh-shikder <mjhsikder@gmail.com>
2026-01-29 16:19:01 +0600

    Add feedback from ui function added

--- src/components/forms/FeedbackForm.jsx
+++ src/components/forms/FeedbackForm.jsx
@@ -1,25 +1,44 @@
-"use-client";
+"use client";
 
 import React from 'react';
 
 const FeedbackForm = () => {
-    return (
-        <div>
-            
-            <form className='space-y-5 text-center'>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const message = e.target.message.value
+        const res = await fetch("http://localhost:3000/api/feedback/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                
+            },
+            body: JSON.stringify({message}),
+        });
 
-                <textarea
-                    required
-                    name=""
-                    id=""
-                    cols={30}
-                    rows={10}
-                    className='w-xl border border-dashed p-3 rounded-xl '
-                ></textarea>
-                <hr />
-                <button className='btn'>Add Feedback</button>
-            </form>
-        </div>
+        const data = await res.json();
+
+        if (data.insertedId) {
+            alert("Success")
+        }
+        
+
+
+    }
+    return (
+      <div>
+        <form onSubmit={handleSubmit} className="space-y-5 text-center">
+          <textarea
+            required
+            name="message"
+            id=""
+            cols={30}
+            rows={10}
+            className="w-xl border border-dashed p-3 rounded-xl "
+          ></textarea>
+          <hr />
+          <button className="btn">Add Feedback</button>
+        </form>
+      </div>
     );
 };
 
