093ebd84fea7fe026e09dd77ebe71e5f4cc90446
mjh-shikder <mjhsikder@gmail.com>
2026-01-29 15:56:31 +0600

    Feedback card added

--- src/app/feedback/add/page.jsx
+++ src/app/feedback/add/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddFeedbackPage = () => {
     return (
         <div>
-            
+            Add Feedbacks
         </div>
     );
 };
--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -1,32 +1,39 @@
-import FeedbackCard from '@/components/cards/FeedbackCard';
-import React from 'react';
+import FeedbackCard from "@/components/cards/FeedbackCard";
+import Link from "next/link";
+import React from "react";
 
 export const metadata = {
-    title: "feedbacks",
+  title: "feedbacks",
 };
 
-
 const getFeedBack = async () => {
-   const res = await fetch("http://localhost:3000/api/feedback/")
-    return await res.json()
-}
+  const res = await fetch("http://localhost:3000/api/feedback/");
+  return await res.json();
+};
 
 const FeedBackPage = async () => {
-    const feedback = await getFeedBack();
-    console.log(feedback);
-    
-    return (
-        <div>
-            <div className="">
-                <h2 className='text-2xl font-bold'>{feedback.length} Feedbacks Found</h2>
-                <div className="my-3 space-y-5">
-                    {feedback.map((fd) => (
-                        <FeedbackCard key={fd._id} feedback={fd}></FeedbackCard>
-                    ))}
-                </div>
-            </div>
+  const feedback = await getFeedBack();
+  console.log(feedback);
+
+  return (
+    <div>
+      <div className="">
+        <h2 className="text-2xl font-bold ">
+          {feedback.length} Feedbacks Found
+        </h2>
+        <div className="my-7 ">
+          <Link href={"/feedback/add"} className="btn ">
+            Add Feedback
+          </Link>
         </div>
-    );
+        <div className="my-3 space-y-5 ">
+          {feedback.map((fd) => (
+            <FeedbackCard key={fd._id} feedback={fd}></FeedbackCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedBackPage;
